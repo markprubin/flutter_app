c0c9022733c6b1e9379e21d32275c39a3bc67264
markprubin <mark.p.rubin@gmail.com>
2023-07-06 09:10:58 -0700

    adds description page, route for page

--- lib/widget/card_widget.dart
+++ lib/widget/card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/class/item_class.dart';
+import 'package:flutter_app/pages/description_page.dart';
 
 import '../core/constants.dart';
 
@@ -12,22 +13,34 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.all(kDouble10),
-        width: double.infinity,
-        child: Column(
-          children: [
-            const SizedBox(height: kDouble5),
-            Image.asset(box.imagePath),
-            Text(box.title,
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                )),
-            Text("This is the ${box.title} description"),
-            const SizedBox(height: kDouble10),
-          ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const DescriptionPage();
+            },
+          ),
+        );
+      },
+      child: Card(
+        child: Container(
+          padding: const EdgeInsets.all(kDouble10),
+          width: double.infinity,
+          child: Column(
+            children: [
+              const SizedBox(height: kDouble5),
+              Image.asset(box.imagePath),
+              Text(box.title,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text("This is the ${box.title} description"),
+              const SizedBox(height: kDouble10),
+            ],
+          ),
         ),
       ),
     );
