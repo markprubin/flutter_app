45d7abb29db9d9927a6b0a21d695979ce2b84b4f
markprubin <mark.p.rubin@gmail.com>
2023-07-09 18:33:20 -0700

    updates profile page contents

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/constants.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: const Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage("images/yeah.png"),
+            radius: 60,
+          ),
+          SizedBox(height: kDouble20),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Flutter Mapp"),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("info@fluttermapp.com"),
+          ),
+          ListTile(
+            leading: Icon(Icons.web),
+            title: Text("www.fluttermapp.com"),
+          ),
+        ],
+      ),
+    );
   }
 }
